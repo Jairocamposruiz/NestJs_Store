124deaa74aabf25aee6e3f81d7c9720383b1b663
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2021-05-31 16:57:35 +0200

    Añadiendo variables de entorno

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hola Mundo!';
+    const apiKey = this.configService.get('API_KEY');
+    const dbName = this.configService.get('DATABASE_NAME');
+    return `Hola Mundo! ${apiKey} ${dbName}`;
   }
 }
