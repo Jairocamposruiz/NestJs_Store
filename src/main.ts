7ba2be2b46738f51def723fea99fcd17ef7840c4
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2021-05-31 17:56:02 +0200

    Añadiendo configuración de Swagger para Autodocumentación

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Validacion de parametros de entrada en body
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  //Autodocumentacion con Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API STORE')
+    .setDescription('STORE NEST JS')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/users/dtos/user.dtos.ts
+++ src/users/dtos/user.dtos.ts
@@ -1,5 +1,5 @@
 import { IsEmail, IsString, IsNotEmpty, Length } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsEmail()
