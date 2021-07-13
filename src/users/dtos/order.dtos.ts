7dda11fa38eb39bdcf776f2a75514ecab5d16203
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2021-07-13 09:40:32 +0200

    Creando el controller, service, entity y dto de Order

---
+++ src/users/dtos/order.dtos.ts
@@ -0,0 +1,30 @@
+import { IsMongoId, IsNotEmpty, IsDate, IsArray } from 'class-validator';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
+
+export class CreateOrderDto {
+  @IsNotEmpty()
+  @IsMongoId()
+  @ApiProperty()
+  readonly customer: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly date: Date;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly products: string[];
+}
+
+export class UpdateOrderDto extends PartialType(
+  OmitType(CreateOrderDto, ['products']),
+) {}
+
+export class AddProductsToOrderDto {
+  @IsArray()
+  @IsNotEmpty()
+  @ApiProperty()
+  readonly productsIds: string[];
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { UsersController } from './controllers/users.controller';
 import { CustomersController } from './controllers/customers.controller';
+import { OrdersController } from './controllers/order.controller';
 
 import { UsersService } from './services/users.service';
 import { CustomersService } from './services/customers.service';
+import { OrdersService } from './services/order.service';
 
 import { Customer, CustomerSchema } from './entities/custormer.entity';
 import { User, UserSchema } from './entities/user.entity';
+import { Order, OrderSchema } from './entities/order.entity';
+import { Skill, SkillSchema } from './entities/skill.entity';
 
 import { ProductsModule } from '../products/products.module';
 
@@ -24,9 +28,17 @@ import { ProductsModule } from '../products/products.module';
         name: User.name,
         schema: UserSchema,
       },
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+      {
+        name: Skill.name,
+        schema: SkillSchema,
+      },
     ]),
   ],
-  controllers: [UsersController, CustomersController],
-  providers: [CustomersService, UsersService],
+  controllers: [UsersController, CustomersController, OrdersController],
+  providers: [CustomersService, UsersService, OrdersService],
 })
 export class UsersModule {}
